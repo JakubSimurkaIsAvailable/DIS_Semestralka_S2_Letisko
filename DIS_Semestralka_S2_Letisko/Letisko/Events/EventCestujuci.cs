c691f4903151a1099625960f5257ce500c7cc9ef
JakubSimurkaIsAvailable <simurka.jakub@gmail.com>
2026-04-01 17:43:05 +0200

    split events into EventCestujuci and EventPrepravka for easier management

---
+++ DIS_Semestralka_S2_Letisko/Letisko/Events/EventCestujuci.cs
@@ -0,0 +1,20 @@
+﻿using DIS_Semestralka_S2_Letisko.Letisko.Actors;
+using DIS_Semestralka_S2_Letisko.Simulation.Actors;
+using DIS_Semestralka_S2_Letisko.Simulation.Event_Based;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS_Semestralka_S2_Letisko.Letisko.Events
+{
+    public abstract class EventCestujuci : Event
+    {
+        public EventCestujuci(LetiskoSimulation core, Cestujuci actor) : base(core, actor)
+        {
+        }
+
+
+    }
+}
---
+++ DIS_Semestralka_S2_Letisko/Letisko/Events/EventPrepravka.cs
@@ -0,0 +1,19 @@
+﻿using DIS_Semestralka_S2_Letisko.Letisko.Actors;
+using DIS_Semestralka_S2_Letisko.Simulation.Actors;
+using DIS_Semestralka_S2_Letisko.Simulation.Event_Based;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS_Semestralka_S2_Letisko.Letisko.Events
+{
+    public abstract class EventPrepravka : Event
+    {
+        public EventPrepravka(LetiskoSimulation core, Prepravka actor) : base(core, actor)
+        {
+        }
+
+    }
+}
