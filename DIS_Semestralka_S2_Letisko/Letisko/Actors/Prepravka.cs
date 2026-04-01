77544854d46c93703bda54da6d702a0218cf9afb
JakubSimurkaIsAvailable <simurka.jakub@gmail.com>
2026-04-01 17:42:28 +0200

    added prepravka logic

--- DIS_Semestralka_S2_Letisko/Letisko/Actors/Prepravka.cs
+++ DIS_Semestralka_S2_Letisko/Letisko/Actors/Prepravka.cs
@@ -9,5 +9,16 @@ namespace DIS_Semestralka_S2_Letisko.Letisko.Actors
 {
     public class Prepravka : Actor
     {
+        public int ID { get; private set; }
+        public int ID_Cestujuci { get; private set; }
+        public double CasUlozeniaNaPasPredRontgenom { get; set; }
+        public double CasZaciatkuRontgenu { get; set; }
+        public double CasUkonceniaRontgenu { get; set; }
+        public double CasOdberuZPasuPoRontgene { get; set; }
+
+        public Prepravka(int ID_cestujuci, int prepravkaCislo)
+        {
+            ID_Cestujuci = ID_cestujuci;
+        }
     }
 }
