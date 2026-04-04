5ddd74fbd87c4e8541bb9cc6c242edd7cd23361f
JakubSimurkaIsAvailable <simurka.jakub@gmail.com>
2026-04-05 00:04:18 +0200

    interval spolahlivosti

--- DIS_Semestralka_S2_Letisko/Simulation/Collectors/StatisticsCollector.cs
+++ DIS_Semestralka_S2_Letisko/Simulation/Collectors/StatisticsCollector.cs
@@ -10,17 +10,41 @@ namespace DIS_Semestralka_S2_Letisko.Simulation.Collectors
     {
         public int ValueCounter { get; private set; }
         public double Average { get; private set; }
+        public double SumOfSquares { get; private set; }
+
+        public double Variance
+        {
+            get
+            {
+                if (ValueCounter < 2) return 0;
+                return (SumOfSquares / ValueCounter) - (Average * Average);
+            }
+        }
+
+        public double StandardDeviation => Math.Sqrt(Variance);
+
         public StatisticsCollector() 
         { 
             ValueCounter = 0;
             Average = 0;
+            SumOfSquares = 0;
         }
 
         public void AddValue(double value)
         {
+            SumOfSquares += value * value;
             Average = (Average * ValueCounter + value) / (ValueCounter + 1);
             ValueCounter++;
             
+        }
+
+        public (double Lower, double Upper)? GetConfidenceInterval(double z = 1.96)
+        {
+            if (ValueCounter < 30)
+                return null; // malo dat
+
+            double margin = z * StandardDeviation / Math.Sqrt(ValueCounter);
+            return (Average - margin, Average + margin);
         }
     }
 }
